da29fa2aaee29018a6ba98f7185ea167eda8d1bd
Lanmijami <milanvukasinovic5@gmail.com>
2025-09-13 18:00:46 +0200

    Home page updated

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -6,7 +6,26 @@ import Footer from '../../components/Footer/Footer';
 const Home = () => {
   return (
     <div className="container">
-      This is the home page of all of yall friends man
+      <h1 className="home-h1">Imas garije? Blago tebi!</h1>
+      <h2>Dodaj, brisi i menjaj informacije o garijima</h2>
+      <p className="home-p">
+        <i>
+          „Čovek ne može živeti sam. A onaj ko je na svetu sam, taj ili je zver
+          ili bog. Zato tražimo prijatelje. A od prijateljstva se živi kao i od
+          hleba, jer ono hrani srce i dušu, kao što hleb hrani telo.“ - Ivo
+          Andrić
+        </i>
+      </p>
+
+      <p className="home-p">
+        <i>
+          Reč "gari" je nekadašnji novosadski žargon koji se, prema nekim
+          mišljenjima, koristi u smislu "brate" ili "druže", a označava
+          prijateljsko obraćanje u nekadašnjim, kolektivističkim društvima sa
+          manje društvenih razlika.
+        </i>
+      </p>
+
       <Footer></Footer>
     </div>
   );
